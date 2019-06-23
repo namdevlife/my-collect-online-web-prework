70c66b525455f1051d26cce1f7b8512c27aeb738
NAM NGUYEN <namdevlife@gmail.com>
2019-06-23 23:19:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,10 +3,10 @@ collection = ['car', 'bike']
 def my_collect(array)
   i = 0
 while i < array.length
-my_collect(collection) do |auto|
-  lang.upcase
 yield(array[i])
 i += 1
+my_collect(collection) do |auto|
+  lang.upcase
 end
 array
 end
